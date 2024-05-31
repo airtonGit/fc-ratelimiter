7709ed6e372669a3514d7dccd74275c21826d7b5
Airton oplen <airton83@icloud.com>
2024-05-30 21:36:19 -0300

    Add usecase test

--- internal/ratelimiter/ratelimit_service_test.go
+++ internal/ratelimiter/ratelimit_service_test.go
@@ -62,34 +62,3 @@ func Test_rateLimitService_Allow_RequestWaitAllow(t *testing.T) {
 
 	assert.Equal(t, true, allowResult, ipTest)
 }
-
-func Test_rateLimitUsecase_DenyToken(t *testing.T) {
-	//Given
-
-	redisCache := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-
-	redisRepository := database.NewRedisRepository(redisCache)
-	ratelimiterUsecase := NewRateLimiterUsecase(redisRepository)
-
-	//When
-	token := "127.0.0.1"
-	input := AllowRateLimitInputDTO{
-		Token: token,
-		TokenLimit: map[string]int{
-			token: 3,
-		},
-		TokenDuration: time.Second,
-	}
-
-	for i := 0; i < 3; i++ {
-		ratelimiterUsecase.Execute(context.Background(), input)
-	}
-	allowResult := ratelimiterUsecase.Execute(context.Background(), input)
-
-	//Then
-	assert.Equal(t, false, allowResult.Allow, token)
-}
---
+++ internal/ratelimiter/ratelimiter_usecase_test.go
@@ -0,0 +1,40 @@
+package ratelimiter
+
+import (
+	"context"
+	"github.com/airtongit/fc-ratelimiter/internal/infrastructure/database"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_rateLimitUsecase_DenyToken(t *testing.T) {
+	//Given
+	redisCache := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	redisRepository := database.NewRedisRepository(redisCache)
+	ratelimiterUsecase := NewRateLimiterUsecase(redisRepository)
+
+	//When
+	token := "127.0.0.1"
+	input := AllowRateLimitInputDTO{
+		Token: token,
+		TokenLimit: map[string]int{
+			token: 3,
+		},
+		TokenDuration: time.Second,
+	}
+
+	for i := 0; i < 3; i++ {
+		ratelimiterUsecase.Execute(context.Background(), input)
+	}
+	allowResult := ratelimiterUsecase.Execute(context.Background(), input)
+
+	//Then
+	assert.Equal(t, false, allowResult.Allow, token)
+}
